8c996e3fefdb6c0a271f97a98478dc20c0a2da22
= <=>
2024-03-02 16:08:34 +0500

    version 3

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test Version 2");
-        System.out.println("version 2");
+        System.out.println("Version 3");
     }
 }
